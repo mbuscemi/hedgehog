5bdba161802391f1b6b02785d87ee4b229cc09f9
mbuscemi <matthew.buscemi@gmail.com>
2020-04-12 15:08:16 -0700

    added an open button (does not do anything yet)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
+#[macro_use]
+extern crate serde_derive;
+
 use web_view::*;
 
-fn main() {
-    let html_content = "<html><body><h1>Hello, World!</h1></body></html>";
+#[derive(Deserialize)]
+#[serde(tag = "msg")]
+enum Message {
+    OpenFileSelector,
+}
 
+fn main() {
     web_view::builder()
         .title("Hedgehog")
-        .content(Content::Html(html_content))
+        .content(Content::Html(HTML))
         .size(1024, 768)
-        .resizable(false)
+        .resizable(true)
         .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
+        .invoke_handler(|_webview, arg| {
+            match serde_json::from_str(arg) {
+                Ok(message) => {
+                    match message {
+                        Message::OpenFileSelector => {
+                            println!("OpenFileSelector event invoked");
+                            Ok(())
+                        },
+                    }
+                }
+                Err(error) => {
+                    println!("Could not match event from webview: {}", error);
+                    Ok(())
+                }
+            }
+        })
         .run()
         .unwrap();
 }
+
+const HTML: &str = r#"
+<!doctype html>
+<html>
+    <body>
+        <header>
+            <button onclick="external.invoke(JSON.stringify({ msg: 'OpenFileSelector' }))">Open</button>
+        </header>
+        <section id="main-editor"></section>
+        <footer></footer>
+    </body>
+</html>
+"#;
